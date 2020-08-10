24d8dcbed829df8b877d82c8af66343b7100db1c
JCamyre <JWcamry03@gmail.com>
2020-08-09 21:57:24 -0700

    Changed the portfolio stock tracking page, added a new page for stocks that triggered an alert, and moved the stock methods to a separate file.

---
+++ stocktracker/methods.py
@@ -0,0 +1,33 @@
+from time import sleep
+import sys
+# Used to access local module outside of its folder
+sys.path.append('C:/Users/JWcam/Desktop/All_projects/Python-Trading')
+import pytrading 
+from pytrading.live_trading import trending_stocks
+from pytrading.download_tickers import load_biggest_movers
+
+
+def format_stock_info(*tickers):
+	stocks = []
+	for stock_ticker in pytrading.Portfolio(tickers):
+		stock = {}
+		cur_stats = stock_ticker.df_month.iloc[-1]
+		prev_stats = stock_ticker.df_month.iloc[-2]
+		stock['stock_ticker'] = stock_ticker.ticker
+		stock['current_price'] = cur_stats['Close']
+		stock['current_percentage'] = ((cur_stats['Close'] - prev_stats['Close'])/prev_stats['Close'])*100
+		stock['high_percentage'] = ((cur_stats['High'] - prev_stats['Close'])/prev_stats['Close'])*100
+		stock['low_percentage'] = ((cur_stats['Low'] - prev_stats['Close'])/prev_stats['Close'])*100
+		stocks.append(stock)
+	return stocks
+
+
+def get_trending_stocks():
+	a = pytrading.Portfolio(load_biggest_movers()[:51])
+	i = 0
+	while True:
+		if i % 10 == 0:
+			a = pytrading.Portfolio(load_biggest_movers()[:51])
+		trending_stocks(a)
+	return stocks
+# How to make the code run every 90 s
--- stocktracker/views.py
+++ stocktracker/views.py
@@ -1,43 +1,25 @@
 from django.shortcuts import render
-import sys
-# Used to access local module outside of its folder
-sys.path.append('C:/Users/JWcam/Desktop/All_projects/Python-Trading')
-from pytrading import Portfolio, Stock
-from time import sleep
-
-# Can still use the method for detecting stocks, but now it only returns the tickers, rather than printing the info for each stock
-# Database?
-# trending_stocks will return a list of Stock objects
-
-def format_stock_info(*tickers):
-	stocks = []
-	for stock_ticker in Portfolio(tickers):
-		stock = {}
-		cur_stats = stock_ticker.df_month.iloc[-1]
-		prev_stats = stock_ticker.df_month.iloc[-2]
-		stock['stock_ticker'] = stock_ticker.ticker
-		stock['current_price'] = cur_stats['Close']
-		stock['current_percentage'] = ((cur_stats['Close'] - prev_stats['Close'])/prev_stats['Close'])*100
-		stock['high_percentage'] = ((cur_stats['High'] - prev_stats['Close'])/prev_stats['Close'])*100
-		stock['low_percentage'] = ((cur_stats['Low'] - prev_stats['Close'])/prev_stats['Close'])*100
-		stocks.append(stock)
-	return stocks
-
-stocks = format_stock_info(['BNGO', 'MRNA', 'MARA', 'TAOP'])
-
-# while True:
-# 	stocks = format_stock_info(['BNGO', 'MRNA', 'MARA', 'TAOP'])
-# 	print(stocks)
-# 	sleep(45)
+from datetime import datetime
+from .methods import format_stock_info
+from .models import Portfolio
 
 # After you login, loop through your stock tickers using format_stock_info to get the stock info on each
 
 def home(request):
+	# If stocks trigger a signal, add it to the list. Updates the site every whatever time. 
 	context = {
-		'stocks': stocks
+		'stocks': format_stock_info(Portfolio.objects.first().stocks),
+		'yo': lambda: datetime.now().strftime('%H:%M:%S')
 	}
+	print(format_stock_info(Portfolio.objects.first().stocks))
 	return render(request, 'stocktracker/home.html', context)
 
 def about(request):
 	return render(request, 'stocktracker/about.html')
 
+# def alerts(request):
+# 	context = {
+# 		'stocks': format_stock_info()
+# 	}
+
+# 	return render(request, 'stocktracker/alerts.html', context)
