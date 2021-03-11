c9d60293183b5078262bb742be6834dd889c7001
JCamyre <JWcamry03@gmail.com>
2021-03-11 12:51:55 -0800

    Fixed favicon issue.

--- stocktracker/views.py
+++ stocktracker/views.py
@@ -56,7 +56,7 @@ class PortfolioDeleteView(DeleteView): # Deleting a portfolio
 class SearchResultsView(ListView):
 	model = Stock
 	template_name = 'search_results.html'
-	queryset = Stock.objects.filter(ticker__icontains='CLO')
+	queryset = Stock.objects.filter(ticker__icontains='CLO') # ticker__icontains very useful
 
 def about(request):
 	return render(request, 'stocktracker/about.html')
